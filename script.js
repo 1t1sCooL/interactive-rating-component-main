623b4a00baddc5620bab3511cae300a881d6218f
1t1sCooL <mmalabugin@gmail.com>
2025-07-13 17:01:22 +0300

    ended styles + scripts

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn');
+const first = document.querySelector('.select');
+const second = document.querySelector('.second');
+const rating = document.querySelector('.rating');
+const radios = document.querySelectorAll('input[type="radio"]')
+btn.onclick = function(event){
+    event.preventDefault();
+    let selectRating;
+    for(const radio of radios){
+        if(radio.checked){
+            first.classList.toggle('hidden');
+            second.classList.toggle('hidden');
+            selectRating = radio.value;
+            break;
+        }
+    }
+    rating.textContent = selectRating;
+}
